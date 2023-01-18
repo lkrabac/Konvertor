1df35df16728a2f4825ea530951316f74af92307
BEforeSisiMisi <52333490+BEforeSisiMisi@users.noreply.github.com>
2023-01-19 00:20:53 +0100

    Pridal som funkciu pre konverciu videa skrz API

---
+++ apicall.php
@@ -0,0 +1,18 @@
+<?php 
+$type = $_POST['type'];
+$url = $_POST['url'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Konvertor</title>
+</head>
+<body>
+<iframe src="https://convert2mp3s.com/api/single/<?php echo $type ?>?url=<?php echo $url ?>"
+width="75%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+</body>
+</html>
